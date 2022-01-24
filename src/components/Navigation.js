03ddb48dbbcba94a04d9f0ec9a3461c179d70d79
Philip Loera <phil72493@hotmail.com>
2022-01-24 02:06:59 -0600

    add active class

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+//TODO
+//Add Active setting to selected course
+//make collapse a better looking dropdown?
+
+
 
 function Navigation(){
 
     return(
-        <Navbar className="main-navbar" expand="lg">
+        <Navbar className="main-navbar" expand="xl">
                 <Navbar.Brand><Link to="/" className="homelogo">Server Workstation Integration</Link></Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="nav-item-set">
-                    <Nav.Item><Link to="Basics" className="nav-links">Computer Basics</Link></Nav.Item>
-                    <Nav.Item><Link to="Networking" className="nav-links">Networking</Link></Nav.Item>
-                    <Nav.Item><Link to="Vmware" className="nav-links">VMWare</Link></Nav.Item>
-                    <Nav.Item><Link to="Windows" className="nav-links">Windows</Link></Nav.Item>
-                    <Nav.Item><Link to="Linux" className="nav-links">Linux</Link></Nav.Item>
-                    <Nav.Item><Link to="Troubleshooting" className="nav-links">Troubleshooting</Link></Nav.Item>
+                        <Nav.Item><NavLink to="Basics" className={({isActive}) => "nav-links" + (isActive ?' selected' : '')} activeClassName="selected">Computer Basics</NavLink></Nav.Item>
+                        <Nav.Item><NavLink to="Networking" className={({isActive}) => "nav-links" + (isActive ?' selected' : '')} activeClassName="selected">Networking</NavLink></Nav.Item>
+                        <Nav.Item><NavLink to="Vmware" className={({isActive}) => "nav-links" + (isActive ?' selected' : '')} activeClassName="selected">VMWare</NavLink></Nav.Item>
+                        <Nav.Item><NavLink to="Windows" className={({isActive}) => "nav-links" + (isActive ?' selected' : '')} activeClassName="selected">Windows</NavLink></Nav.Item>
+                        <Nav.Item><NavLink to="Linux" className={({isActive}) => "nav-links" + (isActive ?' selected' : '')} activeClassName="selected">Linux</NavLink></Nav.Item>
+                        <Nav.Item><NavLink to="Troubleshooting" className={({isActive}) => "nav-links" + (isActive ?' selected' : '')} activeClassName="selected">Troubleshooting</NavLink></Nav.Item>
                 </Navbar.Collapse>
         </Navbar>
     )
