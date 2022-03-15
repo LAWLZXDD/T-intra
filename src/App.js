e2bd9447d96b939d7471e36726e5393f03f6e5fe
Philip Loera <phil72493@hotmail.com>
2022-03-14 17:37:26 -0700

    just saving

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import './App.css';
 import Navigation from './components/Navigation';
-import Homepage from './components/Homepage';
-import Basics from './components/Basics';
-import Networking from './components/Networking';
-import Vmware from './components/Vmware';
-import Windows from './components/Windows';
-import Linux from './components/Linux';
-import Troubleshooting from './components/Troubleshooting';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer';
+
 
 
 
 function App() {
   return (
-    <Router>
       <div className="App">
         <div className="content">
           <Navigation />
           <Footer />
-          <Routes>
-            <Route path="/" element={<Homepage />}></Route>
-            <Route path="Basics" element={<Basics />}></Route>
-            <Route path="Networking" element={<Networking />}></Route>
-            <Route path="Vmware" element={<Vmware />}></Route>
-            <Route path="Windows" element={<Windows />}></Route>
-            <Route path="Linux" element={<Linux />}></Route>
-            <Route path="Troubleshooting" element={<Troubleshooting />}></Route>
-          </Routes>
-          
-          
         </div>
       </div>
-    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Homepage from './components/Homepage';
+import Basics from './components/Basics';
+import Networking from './components/Networking';
+import Vmware from './components/Vmware';
+import Windows from './components/Windows';
+import Linux from './components/Linux';
+import Troubleshooting from './components/Troubleshooting';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <App />
+    <Routes>
+      <Route path="/" element={<Homepage />}></Route>
+      <Route path="Basics" element={<Basics />}></Route>
+      <Route path="Networking" element={<Networking />}></Route>
+      <Route path="Vmware" element={<Vmware />}></Route>
+      <Route path="Windows" element={<Windows />}></Route>
+      <Route path="Linux" element={<Linux />}></Route>
+      <Route path="Troubleshooting" element={<Troubleshooting />}></Route>
+    </Routes>
+    
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
